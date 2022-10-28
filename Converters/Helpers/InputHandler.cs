ebaa0c05d2f04379f5add588c53e41e0e06d191d
mannaayd <mannaayd@fit.cvut.cz>
2022-10-28 19:53:22 +0200

    added input handling class

---
+++ Converters/Helpers/InputHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace Converters;
+
+public class InputHandler
+{
+    /**
+     * @param string inputValueUnit  - given value and unit
+     * @param string inputTargetUnit - target unit
+     * @return value in target unit
+     */
+    public static string HandleInput(string inputValueUnit, string inputTargetUnit)
+    {
+        return "1";
+    }
+
+    // private void GetUnitAndValue(string givenValueUnit, out Unit unit, out double value)
+    // {
+    //     
+    // }
+
+    // private void GetUnit(string givenUnit, out Unit unit)
+    // {
+    //     
+    // }
+
+    // private UnitPrefix GetUnitPrefix(string givenUnit)
+    // {
+    // }
+
+    private bool CheckInputValue(string value)
+    {
+        return true;
+    }
+}
