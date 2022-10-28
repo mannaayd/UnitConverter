531be377e534e43e01ee43e1c6079bc9d06d8f06
mannaayd <mannaayd@fit.cvut.cz>
2022-10-28 19:54:22 +0200

    added unit entities

---
+++ Converters/Entities/BaseSIUnit.cs
@@ -0,0 +1,12 @@
+﻿namespace Converters;
+
+public abstract class BaseSIUnit 
+{
+    public SIUnitPrefix Prefix { get; set; }
+    public double Value { get; set; }
+
+    public double GetValueWithoutPrefix()
+    {
+        return Value * Math.Pow(10, (int)Prefix);
+    }
+}
---
+++ Converters/Enums/UnitPrefix.cs
@@ -0,0 +1,16 @@
+﻿namespace Converters;
+
+public enum SIUnitPrefix
+{
+    EMPTY,
+    DECA,
+    HECTO,
+    KILO,
+    MEGA,
+    GIGA,
+    TERA,
+    PETA,
+    EXA,
+    ZETTA,
+    YOTTA
+}
